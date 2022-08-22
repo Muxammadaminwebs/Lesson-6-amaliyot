913c3f653619ff9e7988829a81150aa3e23befa4
Muxammadaminwebs <qobiljonovmuxammadamin@gmail.com>
2022-08-23 00:05:17 +0500

    lesson-5 uy ishi

---
+++ JavaScript/style.js
@@ -0,0 +1,117 @@
+"use-strick"
+
+
+// 5-masala //
+
+// let n = 5;
+// let arr = [];
+// let sum = 0;
+// for (let i = 0; i <= n; i++) {
+//     let pr = +prompt(`sonni kiriting ${i}`)
+//     arr[i] = pr;
+// }
+//   for (let j = 0; j < arr.length; j++){
+//         sum += arr[j];
+//     }
+// console.log(sum);
+// console.log(arr);
+
+
+
+// 6-masala //
+
+// let n = +prompt('sonni kiriting');
+// let arr = [];
+// for (let i = 0; i <= n; i++){
+//     arr[i] = i;
+// }
+// arr.reverse()
+// console.log(arr);
+
+
+
+// 8-masala //
+
+// let arr = [2, 4, 56, 89, 10, 23, 45, 69];
+// let n1 = 2;
+// let n2 = 4;
+// let sum = 0;
+
+//   for (let j = n1; j <= n2; j++){
+//             sum += arr[j]
+//         }
+// console.log(sum);
+
+// 9-masala //
+
+// let n = 5;
+// let sum = 0;
+// for (let i = 0; i <= n; i++){
+//     if (i % 2 == 0) {
+//         sum = i;
+//     }
+// }
+// console.log(sum);
+
+// 10-masala //
+
+
+// let n = +prompt('sonii-kiriting');
+// let sum = 0;
+// for (let i = n; i >= 0; i--){
+//     if (i % 2 == 0) {
+//         sum = i;
+//     }
+// }
+// console.log(sum);
+
+
+// 11-masala //
+
+
+// let n = 7;
+// let sum = 0;
+// for (let i = 0; i <= n; i++){
+//     if (i % 2 == 1) {
+//         sum = i;
+//     }
+// }
+// console.log(sum);
+
+
+// 12-masala //
+
+// let arr = [2, 4, 56, 89, 10, 23, 45, 69];let arr = [2, 4, 56, 89, 10, 23, 45, 69];
+// let sum = 0;
+// let arrLenght = arr.length;
+
+// for (let i = 0; i < arr.length; i++){
+//     sum+=arr[i]
+// }
+// console.log(sum / arr.length);
+
+// 13-masala //
+
+// let arr = [2, 4, 56, 89, 10, 23, 45, 69];
+// let arrtoq = []
+// for (let i = 0; i <= arr.length; i++){
+//     if (i % 2 == 0) {
+//         continue
+//     }
+//     arrtoq.push(arr[i])
+    
+// }
+// console.log(arrtoq)
+
+// 14-masala //
+
+// 15-masala //
+
+
+// let arr = [2, 4, 56, 89, 10, 23, 45, 69];
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i]%2==1) {
+//         console.log(arr[i]);
+//     }
+        
+// }
